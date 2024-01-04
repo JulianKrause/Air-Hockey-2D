e803dd44c0260497979dc175b9f5f26c2fc3b25f
julian_krause <82316899+JulianKrause@users.noreply.github.com>
2024-01-04 02:45:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-ctx.fillStyle = "#ff0000"
-ctx.fillRect(100, 100, 50, 50);
-
-ctx.beginPath();
-c
